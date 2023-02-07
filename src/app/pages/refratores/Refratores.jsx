ac51743015b99f3cc3bc650085694ee14324e06b
Jonatas Lima <110483641+jonataspl@users.noreply.github.com>
2023-02-06 22:50:26 -0300

    Add button on item

--- src/app/pages/refratores/Refratores.jsx
+++ src/app/pages/refratores/Refratores.jsx
@@ -5,7 +5,7 @@ import { Grid } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
-import { width } from "@mui/system";
+import { useAppThemeContext } from "../../shared/contexts/ThemeContext";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: (theme.palette.mode = "#848586"),
@@ -19,33 +19,62 @@ const Item = styled(Paper)(({ theme }) => ({
 export const Refratores = () => {
   const history = useNavigate();
 
+  const { toggleTheme } = useAppThemeContext();
+
   function homeButton() {
     history("/pagina-inicial");
   }
 
   return (
     <div>
+      <Button variant="contained" onClick={toggleTheme}>
+        Mudar Tema
+      </Button>
       <h1>Refratores</h1>
       <Button variant="contained" onClick={homeButton}>
         Home
       </Button>
-      <Box sx={{ width: "fixed", margin: "10%" }}>
+      <Box sx={{ width: "fixed", margin: "10%", flexGrow: 1 }}>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          <Grid xs={2} sm={4}>
-            <Item sx={{}}>
-              <div>
-                <img
-                  src="https://www.google.com/logos/doodles/2023/celebrating-bubble-tea-6753651837109839.4-l.webp"
-                  alt="some value"
-                />
-              </div>
+          <Grid xs={8} sm={4}>
+            <Item>
+              <img
+                src="https://www.google.com/logos/doodles/2023/celebrating-bubble-tea-6753651837109839.4-l.webp"
+                alt="flex"
+                width={"100%"}
+                height={"100%"}
+              />
               <h3>Produtos</h3>
               <p>Descrição</p>
               <p>Valor</p>
+              <Button
+                variant="contained"
+                sx={{ width: "100%", height: "100%" }}
+              >
+                Adicionar
+              </Button>
+            </Item>
+
+            <Item sx={{}}>
+              <img
+                src="https://www.google.com/logos/doodles/2023/celebrating-bubble-tea-6753651837109839.4-l.webp"
+                alt="flex"
+                width={"100%"}
+                height={"100%"}
+              />
+              <h3>Produtos</h3>
+              <p>Descrição</p>
+              <p>Valor</p>
+              <Button
+                variant="contained"
+                sx={{ width: "100%", height: "100%" }}
+              >
+                Adicionar
+              </Button>
             </Item>
           </Grid>
         </Grid>
