23d4963bea2af2dc7a3342f0bf20fc83dd3fc6fb
Jonatas Lima <110483641+jonataspl@users.noreply.github.com>
2023-03-10 13:06:13 -0300

    Breadcrumbs

--- src/app/pages/refratores/Refratores.jsx
+++ src/app/pages/refratores/Refratores.jsx
@@ -1,31 +1,10 @@
 import "./components/styled.css";
 import { ItemBox } from "./components/itemBox";
 import { useNavigate } from "react-router-dom";
-import { emphasize, styled } from "@mui/material/styles";
-import Chip from "@mui/material/Chip";
+import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
 import { useAppThemeContext } from "../../shared/contexts/ThemeContext";
-import { Breadcrumbs } from "@mui/material";
-
-const StyledBreadcrumb = styled(Chip)(({ theme }) => {
-  const backgroundColor =
-    theme.palette.mode === "light"
-      ? theme.palette.grey[100]
-      : theme.palette.grey[800];
-  return {
-    backgroundColor,
-    height: theme.spacing(3),
-    color: theme.palette.text.primary,
-    fontWeight: theme.typography.fontWeightRegular,
-    "&:hover, &:focus": {
-      backgroundColor: emphasize(backgroundColor, 0.06),
-    },
-    "&:active": {
-      boxShadow: theme.shadows[1],
-      backgroundColor: emphasize(backgroundColor, 0.12),
-    },
-  };
-});
+import Breadcrumbs from "@mui/material/Breadcrumbs";
 
 export const Refratores = () => {
   const history = useNavigate();
@@ -43,18 +22,34 @@ export const Refratores = () => {
           Mudar Tema
         </Button> */}
         <h1 className="portal">JL Astronomia</h1>
+
         {/* <Button variant="contained" onClick={homeButton}>
           Home
         </Button> */}
-
-        <StyledBreadcrumb
-          className="Navegation1"
-          component="a"
-          href="#"
-          label="Nav"
-        />
       </main>
       <body>
+        <div role="presentation" className="Breadcrumbs">
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+              Como funciona
+            </Link>
+            <Link
+              underline="hover"
+              color="inherit"
+              href="/material-ui/getting-started/installation/"
+            >
+              Página inicial
+            </Link>
+            <Link
+              underline="hover"
+              color="text.primary"
+              href="/material-ui/react-breadcrumbs/"
+              aria-current="page"
+            >
+              Formas de pagamento
+            </Link>
+          </Breadcrumbs>
+        </div>
         <p className="alertaDev">Site em desenvolvimento</p>
         <ItemBox />
       </body>
