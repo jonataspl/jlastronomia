f6343cd26d0f3864aee1a9d343f75461bb7cbf6f
Jonatas Lima <110483641+jonataspl@users.noreply.github.com>
2023-03-09 23:14:34 -0300

    Navegation Button add

--- src/app/pages/refratores/Refratores.jsx
+++ src/app/pages/refratores/Refratores.jsx
@@ -1,8 +1,31 @@
 import "./components/styled.css";
 import { ItemBox } from "./components/itemBox";
 import { useNavigate } from "react-router-dom";
+import { emphasize, styled } from "@mui/material/styles";
+import Chip from "@mui/material/Chip";
 import Button from "@mui/material/Button";
 import { useAppThemeContext } from "../../shared/contexts/ThemeContext";
+import { Breadcrumbs } from "@mui/material";
+
+const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+  const backgroundColor =
+    theme.palette.mode === "light"
+      ? theme.palette.grey[100]
+      : theme.palette.grey[800];
+  return {
+    backgroundColor,
+    height: theme.spacing(3),
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightRegular,
+    "&:hover, &:focus": {
+      backgroundColor: emphasize(backgroundColor, 0.06),
+    },
+    "&:active": {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(backgroundColor, 0.12),
+    },
+  };
+});
 
 export const Refratores = () => {
   const history = useNavigate();
@@ -23,6 +46,13 @@ export const Refratores = () => {
         {/* <Button variant="contained" onClick={homeButton}>
           Home
         </Button> */}
+
+        <StyledBreadcrumb
+          className="Navegation1"
+          component="a"
+          href="#"
+          label="Nav"
+        />
       </main>
       <body>
         <p className="alertaDev">Site em desenvolvimento</p>
