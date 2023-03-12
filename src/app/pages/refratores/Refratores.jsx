54af5bb59d98556157324ab2fc02007dbbb5c7a7
Jonatas Lima <110483641+jonataspl@users.noreply.github.com>
2023-03-11 22:35:57 -0300

    Cart icon

--- src/app/pages/refratores/Refratores.jsx
+++ src/app/pages/refratores/Refratores.jsx
@@ -5,6 +5,8 @@ import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
 import { useAppThemeContext } from "../../shared/contexts/ThemeContext";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
+import IconButton from "@mui/material/IconButton";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 export const Refratores = () => {
   const history = useNavigate();
@@ -21,7 +23,12 @@ export const Refratores = () => {
         {/* <Button variant="contained" onClick={toggleTheme} className="toggle">
           Mudar Tema
         </Button> */}
-        <h1 className="portal">JL Astronomia</h1>
+        <h1 className="portal">
+          JL Astronomia
+          <IconButton style={{ display: "flex", justifyContent: "center" }}>
+            <AddShoppingCartIcon className="CarrinhoIco" />
+          </IconButton>
+        </h1>
 
         {/* <Button variant="contained" onClick={homeButton}>
           Home
@@ -34,7 +41,7 @@ export const Refratores = () => {
               Como funciona
             </Link>
             <Link underline="hover" color="text.primary" href="/">
-              Página inicial
+              Recomendações e Feedbacks
             </Link>
             <Link underline="hover" color="text.primary" href="/">
               Formas de pagamento
